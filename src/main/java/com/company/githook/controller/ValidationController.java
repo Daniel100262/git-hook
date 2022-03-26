713a993b732e98fdb2fc12ea098a23fa10323bf9
daniel100262 <daniel100262@hotmail.com>
2022-03-26 02:52:00 -0300

    Added API run test endpoint

---
+++ src/main/java/com/company/githook/controller/ValidationController.java
@@ -0,0 +1,17 @@
+package com.company.githook.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/validate")
+public class ValidationController {
+	
+	@GetMapping
+	public ResponseEntity<String> validate() {
+		return ResponseEntity.ok("The API is running!");
+	}
+	
+}
