f1ed60d77d1a8c8a8d3ffcff0ad8e7c512750e7b
daniel100262 <daniel100262@hotmail.com>
2022-03-26 02:45:07 -0300

    First commit

---
+++ src/main/java/com/company/githook/GitHookApplication.java
@@ -0,0 +1,13 @@
+package com.company.githook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHookApplication.class, args);
+	}
+
+}
